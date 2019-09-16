25c24e360edf1ab9edae11a07e25d052eb4444e2
Santiago Semhan <santiagosemhan@gmail.com>
2019-09-16 13:17:51 -0300

    Delete unused files.

--- src/redux/estudios/actions.js
+++
@@ -1,9 +0,0 @@
-const actions = {
-  SET_STATE: 'estudios/SET_STATE',
-  GET_DATA: 'estudios/GET_DATA',
-  GET_CURRENT: 'estudios/GET_CURRENT',
-  UPDATE: 'estudios/UPDATE',
-  CREATE: 'estudios/CREATE',
-}
-
-export default actions
--- src/redux/investigadores/actions.js
+++
@@ -1,9 +0,0 @@
-const actions = {
-  SET_STATE: 'investigadores/SET_STATE',
-  GET_DATA: 'investigadores/GET_DATA',
-  GET_CURRENT: 'investigadores/GET_CURRENT',
-  UPDATE: 'investigadores/UPDATE',
-  CREATE: 'investigadores/CREATE',
-}
-
-export default actions
