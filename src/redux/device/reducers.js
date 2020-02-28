e794d0fcabb2c00decebb42b8bbf22aa049017f4
santiagosemhan <santiagosemhan@gmail.com>
2020-02-28 15:29:44 -0300

    Remove comments

--- src/redux/device/reducers.js
+++ src/redux/device/reducers.js
@@ -6,28 +6,6 @@ const initialState = {
   lastError: null,
 }
 
-/* export default function deviceReducers( state = initialState, action ) {
-  switch (action.type) {
-    case actions.REQUEST_KEY:
-      return Object.assign({}, state, {
-        isFetching: true,
-        didInvalidate: false
-      })
-    case actions.RECEIVE_KEY:
-      return Object.assign({}, state, {
-        isFetching: false,
-        didInvalidate: false,
-        deviceKey: action.payload.key,
-        lastUpdated: action.payload.receivedAt
-      })
-    case actions.CLEAR_KEY:
-      return Object.assign({}, state, action.payload);
-    default:
-      return Object.assign({}, state, action.payload);
-  }
-}
- */
-
 export default function userReducer(state = initialState, action) {
   switch (action.type) {
     case actions.SET_STATE:
